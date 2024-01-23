0b85cf9cebd24f2191c86d62e76be30337c441e0
RohitRaj23 <rohitraj23092002@gmail.com>
2024-01-24 00:19:36 +0530

    changes in server.js and env

--- backend/server.js
+++ backend/server.js
@@ -37,17 +37,21 @@ app.use('/api/messages',messageRoutes)
 const __dirname1 = path.resolve();
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, "./frontend/build")));
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname1, "./frontend/build")));
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-    });
-} else {
-    app.get('/',(req,res) => {
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+//     });
+// } else {
+//     app.get('/',(req,res) => {
+//     res.send("on 5000 port ")
+// })
+// }
+
+app.get('/',(req,res) => {
     res.send("on 5000 port ")
 })
-}
 
 // ----------------------------deployment--------------------------------------
 
